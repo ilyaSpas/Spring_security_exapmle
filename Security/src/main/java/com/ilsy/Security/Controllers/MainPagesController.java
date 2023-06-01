e3ca3df1451b169fa75c3e818a7a06c2995703f8
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2023-06-01 17:30:33 +0300

    New commit

--- Security/src/main/java/com/ilsy/Security/Controllers/MainPagesController.java
+++ Security/src/main/java/com/ilsy/Security/Controllers/MainPagesController.java
@@ -8,8 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,6 +40,56 @@ public class MainPagesController {
         }
         return "blog";
     }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> news = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        news.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blogDetails";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional <Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blogEdit"; //
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogAddPostUpdate(@PathVariable(value = "id") long id,
+                                    @RequestParam String title,
+                                    @RequestParam String anons,
+                                    @RequestParam String text) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setText(text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+
+
+
+
+
+    @PostMapping("/blog/{id}/remote")
+    public String blogAddPostDelete(@PathVariable(value = "id") long id, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
+
     @GetMapping("/about")
     public String aboutPage(Model model, Principal principal) {
         if(principal != null) {
